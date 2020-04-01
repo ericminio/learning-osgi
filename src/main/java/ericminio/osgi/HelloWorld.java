9c68c0be874298d52839d5886be9e21c5f3b17ad
Eric Mignot <eric.mignot@gmail.com>
2020-03-31 19:05:51 -0700

    Create HelloWorld.java

---
+++ src/main/java/ericminio/osgi/HelloWorld.java
@@ -0,0 +1,16 @@
+package ericminio.osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class HelloWorld implements BundleActivator {
+
+    public void start(BundleContext ctx) {
+        System.out.println("Hello World.");
+    }
+
+    public void stop(BundleContext bundleContext) {
+        System.out.println("Goodbye World.");
+    }
+
+}
